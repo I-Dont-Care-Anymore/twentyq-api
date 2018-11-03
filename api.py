173f485f119bfa0a0cab9f063bda947546d743e1
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-03 01:30:36 -0500

    Add beautiful soup 4 and pylint

---
+++ api.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+from flask import Flask
+app = Flask(__name__)
+ 
+@app.route("/")
+def hello():
+    return "Hello World!"
+ 
+if __name__ == "__main__":
+    app.run()
