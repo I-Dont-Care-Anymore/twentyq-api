a8ae903ac4cfe5894f28e0e5262e8d84f212754f
Daiwei Lu <daiwei.lu@vanderbilt.edu>
2018-11-03 16:15:32 -0500

    store stock info in tables

---
+++ microstrat.py
@@ -0,0 +1,11 @@
+#from mstrio import microstrategy
+#conn = microstrategy.Connection(base_url="https://acmeinc.mstr.com/MicroStrategyLibrary/api", username="myUsername", password="myPassword", project_name="Acme, Inc. Analytics")
+
+#conn.connect()
+
+
+
+
+import stocklookup
+
+stocklookup.loadStocks("Microsoft")
