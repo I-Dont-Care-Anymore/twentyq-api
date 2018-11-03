eea3054871db9ac62860cd7beeb3a0b42da301f8
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-03 12:06:33 -0500

    Make data structures for NAICS data from Excel files & wipe api.py

---
+++ naics.py
@@ -0,0 +1,18 @@
+from typing import Dict, List
+from collections import namedtuple
+
+import pandas as pd
+import numpy as np
+
+CompanyInfo = namedtuple('CompanyInfo', ['Address', 'CompanyDUNSNumber', 'CorporateName', 'EmployeesOnSite', 'Latitude', 'Longitude', 'NAICS1', 'NAICS1Name', 'NAICS2', 'NAICS2Name',
+                                         'PointofContact', 'PublicOrPrivate', 'SIC1', 'SIC1Name', 'SIC2', 'SIC2Name', 'SalesVolume', 'Telephone', 'Title', 'TotalEmployees', 'TradestyleName', 'YearStarted'])
+
+company_infos: List[CompanyInfo] = []
+
+for tup in pd.read_excel('NAICSData.xlsx').itertuples(name='CompanyInfo', index=False):
+    company_infos.append(tup)
+
+cross_references: Dict[int, str] = {}
+
+for tup in pd.read_excel('2017_NAICS_Cross_References.xlsx').itertuples():
+    cross_references[tup.Code] = tup.CrossReference
