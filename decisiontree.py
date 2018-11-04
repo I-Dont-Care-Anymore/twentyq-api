c7c9da5e2cd95745007413a279f8baa67ce44f03
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-04 04:51:27 -0600

    Try fixing the cors woes

--- decisiontree.py
+++ decisiontree.py
@@ -95,4 +95,4 @@ for code, doc in cross_references.items():
     categories_tags_dict[code] = tag_freqs
 
 questions_tree = TreeClassifier(
-    [token_norm for token_norm in attributes.keys()], categories_tags_dict, max_depth=10)
+    [token_norm for token_norm in attributes.keys()], categories_tags_dict, max_depth=3)
