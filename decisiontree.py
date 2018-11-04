752dd3ad102d85ca26886c08b7aeeffc18e53c19
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-04 06:16:56 -0600

    syntax

--- decisiontree.py
+++ decisiontree.py
@@ -68,7 +68,7 @@ class TreeClassifier(object):
         self.split_metric = split_metric
         root_value = splitting_metrics.even(attributes, categories_tags_dict, 0.5, {}, {})
         attributes.remove(root_value)
-        self.root = Node(max_depth=max_depth, attributes=attributes, categories_tags_dict=categories_tags_dict, value=root_value), parent=None)
+        self.root = Node(max_depth=max_depth, attributes=attributes, categories_tags_dict=categories_tags_dict, value=root_value, parent=None)
 
     def print_tree(self):
         print(self.root)
