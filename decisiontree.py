048726d10b8e421dbbd77d4d43a5e1420a41f80b
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-04 08:06:25 -0600

    Whew

--- decisiontree.py
+++ decisiontree.py
@@ -47,7 +47,7 @@ class Node(object):
             right: splitting_metrics.CategoryFreqs = {}
 
             new_node_value = splitting_metrics.even(
-                self.attributes, self.categories_tags_dict, 0.25, left, right)
+                self.attributes, self.categories_tags_dict, 0.5, left, right)
 
             attributes_copy = self.attributes.copy()
             attributes_copy.remove(new_node_value)
@@ -67,7 +67,7 @@ class TreeClassifier(object):
 
     def __init__(self, attributes: splitting_metrics.Attributes, categories_tags_dict: splitting_metrics.CategoryFreqs, max_depth: int):
         root_value = splitting_metrics.even(
-            attributes, categories_tags_dict, 0.25, {}, {})
+            attributes, categories_tags_dict, 0.5, {}, {})
         attributes.remove(root_value)
         for tag_value_map in categories_tags_dict.values():
             tag_value_map.pop(root_value)
