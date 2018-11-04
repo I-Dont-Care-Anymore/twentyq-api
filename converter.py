6113ec48eeb884e526c34dd5767c7f76cc872d54
Daiwei Lu <daiwei.lu@vanderbilt.edu>
2018-11-04 07:28:26 -0600

    chages to converter?

--- converter.py
+++ converter.py
@@ -3,12 +3,12 @@ import stocklookup
 import json
 
 def passNAICS(number):    
+     excelData = json.dumps(excelsearcher(number) ) 
+     stockData = json.dumps(excelData["comp0"] ) 
+     for i in range(1, 10): 
+         stockData = stockData.concat(json.dumps(excelData["comp"+ str(i)]) ) 
+     returnedData = excelData.concat(stockData ) 
+     return returnedData
+
+
      
-   
-    
-    
-    
-    
-    
-    
-   = excelsearcher(number)
