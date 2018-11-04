cbebbd35e60b95a10c65a55ceddc59ef629ea2ad
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-03 23:47:08 -0500

    https let's get it wooo

--- scripts/run_ubuntu.sh
+++ scripts/run_ubuntu.sh
@@ -1,1 +1,1 @@
-sudo su -c 'cd /home/ubuntu/twentyq-api && source .env/bin/activate && gunicorn main:app --bind 0.0.0.0:80'
+sudo su -c 'cd /home/ubuntu/twentyq-api && source .env/bin/activate && gunicorn main:app --bind 0.0.0.0:443 --keyfile=/etc/letsencrypt/live/api.twentyq.com/privkey.pem --certfile=/etc/letsencrypt/live/api.twentyq.com/cert.pem --ca_certs=/etc/letsencrypt/live/api.twentyq.com/chain.pem'
